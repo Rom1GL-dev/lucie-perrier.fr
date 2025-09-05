6e231146ae146ed35a5690837ddd80226d2edb1c
RomainEntreCodeurs <romain@lesentrecodeurs>
2025-09-06 00:43:53 +0200

    feat():

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import { Banner } from '@/components/ux/banner';
 import Navbar from '@/components/ux/navbar';
 import { Footer } from '@/components/ux/footer';
 
 export default function VitrineLayout({
-                                     children,
-                                   }: Readonly<{
+  children
+}: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      .next-dev-overlay-button {
+        display: none !important;
+      }
+    `;
+    document.head.appendChild(style);
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Banner />
       <Navbar />
-      <main className="flex-grow">
-        {children}
-      </main>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
